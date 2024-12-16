9a846a177f8c6c2014202c8afbd952c688608d7a
azolanda <daemonzhilkevich@gmail.com>
2024-12-17 02:44:29 +0300

    feat: some tasks done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -240,8 +240,14 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  return `${minutes} + : + ${seconds}`;
 }
 
 /**
